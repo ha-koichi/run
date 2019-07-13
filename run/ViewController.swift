d9c1aeb3a65763adcfec8b4722326f8f3692d636
ha-koichi <koichi@Hako.local>
2019-07-13 11:42:12 +0900

    first commit

---
+++ run/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  run
+//
+//  Created by Ha-koichi on 2019/07/13.
+//  Copyright © 2019 Ha-koichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
