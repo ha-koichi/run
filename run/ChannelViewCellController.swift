dcad6d86a30586b839f840713cf982bc6a9efc81
ha-koichi <koichi@Hako.local>
2019-07-28 22:13:24 +0900

    xibによるtableviewの作成

---
+++ run/ChannelViewCellController.swift
@@ -0,0 +1,28 @@
+//
+//  ChannelViewCellController.swift
+//  run
+//
+//  Created by Ha-koichi on 2019/07/21.
+//  Copyright © 2019 Ha-koichi. All rights reserved.
+//
+
+import UIKit
+
+class ChannelViewCellController: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- run/ViewController.swift
+++ run/ViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
+    @IBOutlet weak var tbl: UITableView!
+    
+    let data = [("タイトル1", "田中", "記事の内容1"), ("タイトル2", "鈴木", "記事の内容2"), ("タイトル3", "佐藤", "記事の内容3")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tbl.dataSource = self
+        tbl.delegate = self
+        tbl.register(UINib(nibName: "ChannelViewCellController", bundle: nil), forCellReuseIdentifier: "ChannelViewCellController")
+
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(data.count)
+        return data.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let now = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChannelViewCellController", for: indexPath ) as! ChannelViewCellController
+
+        // セルに表示する値を設定する
+        cell.titleLabel.text = data[indexPath.row].0
+        cell.nameLabel.text = data[indexPath.row].1
+        cell.dateLabel.text = formatter.string(from: now as Date)
+//        cell.contentText.text = data[indexPath.row].2
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
